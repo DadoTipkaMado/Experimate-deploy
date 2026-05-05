b5a018b5a5332d21d18bc0d09bb30bbaa6191e17
DadoTipkaMado <codedavidtopic@gmail.com>
2026-05-05 16:15:36 +0200

    Feature: Rate limiting on endpoints towards Claude API (anti-spam) 	Add: RateLimiterService, RateLimitOperation, RateLimitException 	Add: Rate limit triggering in MatchController endpoints 	Add: Exception handler for new exception in GlobalExceptionHandler

---
+++ src/main/java/hr/tvz/experimate/experimate/model/shared/RateLimitOperation.java
@@ -0,0 +1,25 @@
+package hr.tvz.experimate.experimate.model.shared;
+
+import io.github.bucket4j.Bandwidth;
+
+import java.time.Duration;
+import java.util.List;
+
+public enum RateLimitOperation {
+    AI_SEARCH(Bandwidth.builder()
+            .capacity(10)
+            .refillIntervally(10, Duration.ofHours(1))
+            .build()
+    ),
+    AI_EXPLAIN(Bandwidth.builder()
+            .capacity(10)
+            .refillIntervally(10, Duration.ofMinutes(5))
+            .build()
+    );
+
+    final List<Bandwidth> limits;
+
+    RateLimitOperation(Bandwidth... limits){
+        this.limits = List.of(limits);
+    }
+}
