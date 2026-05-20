2e626ff814b16c2271ecb1a88a1c2f80fc5430ac
DadoTipkaMado <codedavidtopic@gmail.com>
2026-05-20 20:31:30 +0200

    Fix: add unique to user FK

--- src/main/resources/db/migration/V5__add_verification_and_reset_tokens.sql
+++ src/main/resources/db/migration/V5__add_verification_and_reset_tokens.sql
@@ -1,13 +1,13 @@
 CREATE TABLE email_verification_token (
     id                   SERIAL PRIMARY KEY,
     token_hash           VARCHAR(64) NOT NULL UNIQUE,
-    user_id              INTEGER     NOT NULL REFERENCES app_user(id) ON DELETE CASCADE,
+    user_id              INTEGER     NOT NULL UNIQUE REFERENCES app_user(id) ON DELETE CASCADE,
     expiration_date_time TIMESTAMP   NOT NULL
 );
 
 CREATE TABLE password_reset_token (
     id                   SERIAL PRIMARY KEY,
     token_hash           VARCHAR(64) NOT NULL UNIQUE,
-    user_id              INTEGER     NOT NULL REFERENCES app_user(id) ON DELETE CASCADE,
+    user_id              INTEGER     NOT NULL UNIQUE REFERENCES app_user(id) ON DELETE CASCADE,
     expiration_date_time TIMESTAMP   NOT NULL
 );
